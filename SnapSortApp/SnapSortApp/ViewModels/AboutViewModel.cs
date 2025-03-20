589a311f13d8b958d5ac12bc368720131e0b8b4f
fatematahir10 <fatemaqureshi9@gmail.com>
2025-03-20 14:52:52 +0500

    Added AboutViewModel

---
+++ SnapSortApp/SnapSortApp/ViewModels/AboutViewModel.cs
@@ -0,0 +1,30 @@
+﻿using SnapSortApp.Services;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace SnapSortApp.ViewModels
+{
+    public class AboutViewModel : BaseViewModel
+    {
+        public string AppName => AppInfoHelper.AppName;
+        public string AppVersion => AppInfoHelper.AppVersion;
+        public string Copyright => AppInfoHelper.Copyright;
+        public string HelpLink => AppInfoHelper.HelpLink;
+
+        public ICommand OpenHelpCommand { get; }
+
+        public AboutViewModel()
+        {
+            OpenHelpCommand = new RelayCommand(OpenHelp);
+        }
+
+        private void OpenHelp()
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = HelpLink,
+                UseShellExecute = true
+            });
+        }
+    }
+}
--- SnapSortApp/SnapSortApp/Views/AboutView.xaml.cs
+++ SnapSortApp/SnapSortApp/Views/AboutView.xaml.cs
@@ -1,19 +1,15 @@
 ﻿using SnapSortApp.Services;
+using SnapSortApp.ViewModels;
 using System.Windows.Controls;
 
 namespace SnapSortApp.Views
 {
     public partial class AboutView : UserControl
     {
-        public string AppName => AppInfoHelper.AppName;
-        public string AppVersion => AppInfoHelper.AppVersion;
-        public string Copyright => AppInfoHelper.Copyright;
-        public string HelpLink => AppInfoHelper.HelpLink;
-
         public AboutView()
         {
             InitializeComponent();
-            DataContext = this;
+            DataContext = new AboutViewModel();
         }
     }
 }
